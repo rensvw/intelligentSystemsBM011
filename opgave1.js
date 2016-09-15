cd61f5eeada73e801e2063fb65c54f44a8f864be
Rens van Woudenberg <rensvanw@outlook.com>
2016-09-15 13:48:39 +0200

    Opgave 1 opgelost met insertion sort en divide and conquer

--- opgave1.js
+++ opgave1.js
@@ -8,10 +8,8 @@ function decreaseAndConquerSort(array) {
     }
     else {
         let m = rij[rij.length - 1];
-
         let r = decreaseAndConquerSort(rij.slice(0, rij.length - 1));
         for (let i = 0; i < rij.length; i++) {
-
             if (m <= r[i]) {
                 r.splice(i, 0, m);
                 console.log(r);
