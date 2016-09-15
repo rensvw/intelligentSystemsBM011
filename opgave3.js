66c4f43779be8ad1d4c090d95fd43601ad6cce3d
Rens van Woudenberg <rensvanw@outlook.com>
2016-09-15 14:29:36 +0200

    Opgave 3 opgelost, geeft de kleinst gevonden waarde terug uit de boom

--- opgave3.js
+++ opgave3.js
@@ -4,21 +4,13 @@
 // Opgave 3 //
 
 // BinaryTree Object
+// Storage mapping function
 function BinaryTree() {
 
 
     this.Nodes = new Array();
     this.level = 0;
     this.node = 0;
-
-    // Oude Get en Set methodes
-
-    /*this.setNode=function(value,level,node){
-     this.Nodes[this.btSMF(level,node)]=value;
-     }
-     this.getNode=function(level,node){
-     return this.Nodes[this.btSMF(level,node)];
-     }*/
 
     this.setNode = function (value, level, node) {
         if (level === undefined) {
@@ -79,30 +71,35 @@ function BinaryTree() {
 }
 
 tree = new BinaryTree();
-tree.setNode(1, 0, 0);
-tree.setNode(2, 1, 0);
+tree.setNode(7, 0, 0);
+tree.setNode(4, 1, 0);
 tree.setNode(3, 1, 1);
-tree.setNode(4, 2, 0);
-tree.setNode(5, 2, 1);
-tree.setNode(6, 2, 2);
-tree.setNode(7, 2, 3);
+tree.setNode(6, 2, 0);
+tree.setNode(2, 2, 1);
+tree.setNode(1, 2, 2);
+tree.setNode(8, 2, 3);
 
-//console.log(tree.getNode(2,1));
 
-/*console.log(tree.root());
- console.log(tree.rightChild());
- console.log(tree.rightChild());
- console.log(tree.parent());
- console.log(tree.leftChild());
- console.log(tree.rightChild());*/
+let value = tree.getNode();
 
 function traverse() {
-    console.log(tree.getNode());
-    if (tree.leftChild() !== undefined)traverse();
+    let newValue = tree.getNode();
+    if (value > newValue) {
+        value = tree.getNode();
+    }
+    console.log("Smallest found value = ", value);
+    if (tree.leftChild() !== undefined) {
+        traverse();
+    }
     tree.parent();
-    if (tree.rightChild() !== undefined)traverse();
+    if (tree.rightChild() !== undefined) {
+        traverse();
+    }
     tree.parent();
+
 }
+
+
 
 tree.root();
 traverse();
